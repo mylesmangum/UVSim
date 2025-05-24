ca3ea4023ce10ce66eb6109fde308d9212fc8e56
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2025-05-24 10:41:01 -0600

    Add functionality to the memory class

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Memory memory = new Memory();
-        memory.write(0, 100);
-        System.out.println(memory.read(0));
+
     }
 }
--- Memory.java
+++ Memory.java
@@ -1,13 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Memory {
     int[] memoryArray = new int[100];
-
+    public Memory(String fileName) {
+        if (fileName.endsWith(".txt")) {
+            memoryArray = readText(fileName);
+        }
+    }
     public int read(int address) {
         checkIndex(address);
         return memoryArray[address];
     }
     public void write(int address, int value) {
+        // TODO, refactor to print to screen instead of writing to memory?
+        // I have two ideas in my mind on what this does and I want to clairify
         checkIndex(address);
         memoryArray[address] = value;
+    }
+    public int[] readText(String fileName) {
+        int[] myArray = new int[100];
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+            int i = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                myArray[i] = Integer.parseInt(line);
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.printf("File %s not found", fileName);
+            e.printStackTrace();
+        }
+        return myArray;
     }
 
     private void checkIndex(int address) {
